ec1f306243e4141291e4db5c2759ec2443658a4d
saikishore <chsaikishore4118@gmail.com>
2025-02-28 21:38:03 +0530

    second commit

--- Helloworld/src/com/Hello.java
+++ Helloworld/src/com/Hello.java
@@ -5,6 +5,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloworld");
+		StringBuffer sb = new StringBuffer("virat");
+		sb.append("18");
+		System.out.println(sb);
+
 	}
 
 }
